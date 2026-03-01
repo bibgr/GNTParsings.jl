4c3fe3da7976f16840c94d098ce9bacf82ff5d04
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-03-01 20:48:49 -0300

    UPD getting a world-age pre-error warning on types.jl

--- src/types.jl
+++ src/types.jl
@@ -4,7 +4,57 @@
 The `GNTParsings` module parsing internal representation.
 """
 struct parsing <: GNTParsings
+    # Part of speech
+    # ("A-", "C-", "D-", "I-", "N-", "P-", "RA", "RD", "RI", "RP", "RR", "V-", "X-")
+    pos::String
+    # Person
+    # ('-', '1', '2', '3')
+    per::Char
+    # Tense
+    # ('-', 'A', 'F', 'I', 'P', 'X', 'Y')
+    ten::Char
+    # Voice
+    # ('-', 'A', 'M', 'P')
+    voi::Char
+    # Mood
+    # ('-', 'D', 'I', 'N', 'O', 'P', 'S')
+    moo::Char
+    # Case
+    # ('-', 'A', 'D', 'G', 'N', 'V')
+    cas::Char
+    # Number
+    # ('-', 'P', 'S')
+    num::Char
+    # Gender
+    # ('-', 'F', 'M', 'N')
+    gen::Char
+    # Degree
+    # ('-', 'C', 'S')
+    deg::Char
+    # Internal (validating) constructor
+    function parsing(pos::S,
+                     per::C="-", ten::C="-", voi::C="-", moo::C="-",
+                     cas::C="-", num::C="-", gen::C="-", deg::C="-"
+                    ) where {S<:AbstractString, C<:AbstractChar}
+        @assert(
+            pos in ("A-", "C-", "D-", "I-", "N-", "P-","V-", "X-") ||
+            pos in ("RA", "RD", "RI", "RP", "RR")
+        )
+        @assert per in "-123"
+        @assert ten in "-AFIPXY"
+        @assert voi in "-AMP"
+        @assert moo in "-DINOPS"
+        @assert cas in "-ADGNV"
+        @assert num in "-PS"
+        @assert gen in "-FMN"
+        @assert deg in "-CS"
+        return new(pos, per, ten, voi, moo, cas, num, gen, deg)
+    end
+    # Internal convenience constructors
+    parsing(pos::S, par::S) where S<:AbstractString = parsing(pos, par...)
 end
 
 export parsing
+
+parsing(all::S) where S<:AbstractString = parsing(split(all, ' ')...)
 
