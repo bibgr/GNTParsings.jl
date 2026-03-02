41dd1cfd914166db2a8c907382f401ccb6d9f09a
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-03-02 12:19:20 -0300

    UPD the src/types.jl

--- src/types.jl
+++ src/types.jl
@@ -1,4 +1,106 @@
 """
+`meaning`\n
+A static data `Dict` with keys to various fields meanings
+"""
+meaning = Dict(
+    :pos => Dict(
+        :what => "Part of speech",
+        :code => Dict(
+            "A-" => ("adj.", "adjective"),
+            "C-" => ("conj.", "conjunction"),
+            "D-" => ("adv.", "adverb"),
+            "I-" => ("interj.", "interjection"),
+            "N-" => ("n.", "noun"),
+            "P-" => ("prep.", "preposition"),
+            "RA" => ("art.", "definite article"),
+            "RD" => ("dem.pron.", "demonstrative pronoun"),
+            "RI" => ("int./indef.pron.", "interrogative/indefinite pronoun"),
+            "RP" => ("pers.pron.", "personal pronoun"),
+            "RR" => ("rel.pron.", "relative pronoun"),
+            "V-" => ("v.", "verb"),
+            "X-" => ("part.", "particle"),
+        )
+    ),
+    :per => Dict(
+        :what => "Person",
+        :code => Dict(
+            '-' => ("no p.", "impersonal"),
+            '1' => ("1st p.", "first person"),
+            '2' => ("2nd p.", "second person"),
+            '3' => ("3rd p.", "third person"),
+        ),
+    ),
+    :tense => Dict(
+        :what => "Tense",
+        :code => Dict(
+            '-' => ("no t.", "no tense"),
+            'P' => ("pres.", "present"),
+            'I' => ("impf.", "imperfect"),
+            'F' => ("fut.", "future"),
+            'A' => ("aor.", "aorist"),
+            'X' => ("perf.", "perfect"),
+            'Y' => ("plpf.", "pluperfect"),
+        ),
+    ),
+    :voice => Dict(
+        :what => "Voice",
+        :code => Dict(
+            '-' => ("no v.", "no voice"),
+            'A' => ("act.", "active"),
+            'M' => ("mid.", "middle"),
+            'P' => ("pass.", "passive"),
+        ),
+    ),
+    :mood => Dict(
+        :what => "Mood",
+        :code => Dict(
+            '-' => ("no m.", "no mood"),
+            'I' => ("indic.", "indicative"),
+            'D' => ("imper.", "imperative"),
+            'S' => ("subj.", "subjunctive"),
+            'O' => ("opt.", "optative"),
+            'N' => ("inf.", "infinitive"),
+            'P' => ("ptc.", "participle"),
+        ),
+    ),
+    :case => Dict(
+        :what => "Case",
+        :code => Dict(
+            '-' => ("no c.", "no case"),
+            'N' => ("nom.", "nominative"),
+            'G' => ("gen.", "genitive"),
+            'D' => ("dat.", "dative"),
+            'A' => ("acc.", "accusative"),
+        ),
+    ),
+    :number => Dict(
+        :what => "Number",
+        :code => Dict(
+            '-' => ("no n.", "no number"),
+            'S' => ("sg.", "singular"),
+            'P' => ("pl.", "plural"),
+        ),
+    ),
+    :gender => Dict(
+        :what => "Gender",
+        :code => Dict(
+            '-' => ("no g.", "no gender"),
+            'M' => ("m.", "masculine"),
+            'F' => ("f.", "feminine"),
+            'N' => ("n.", "neuter"),
+        ),
+    ),
+    :degree => Dict(
+        :what => "Degree",
+        :code => Dict(
+            '-' => ("no d.", "no degree"),
+            'C' => ("comp.", "comparative"),
+            'S' => ("sup.", "superlative"),
+        ),
+    ),
+)
+
+"""
 `struct parsing <: GNTParsings`\n
 
 The `GNTParsings` module parsing internal representation.
@@ -44,4 +146,23 @@ parsing(all::S) where S<:AbstractString = begin
     spl = split(all, limit=2)
     parsing(spl[1], join(split(spl[2]), ""))
 end
+
+# Base.show
+import Base: show
+
+function show(io::IO, x::parsing)
+    a = "$(x.pos) "
+    for f in (:per, :ten, :voi, :moo, :cas, :num, :gen, :deg)
+        a *= @eval $x.$f
+    end
+    print(io, "parsing(\"$a\")")
+end
+
+# Explain
+
+function _explain(io::IO, x::parsing)
+    
+end
+
+explain(x::parsing, io::IO = Base.stdout) = _explain(io, x)
 
