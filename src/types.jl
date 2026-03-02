b0b05560a16b2774347972a32eef2572dde50e71
C. Naaktgeboren <NaaktgeborenC.PhD@gmail.com>
2026-03-02 11:29:51 -0300

    UPD the src/types.jl

--- src/types.jl
+++ src/types.jl
@@ -4,49 +4,32 @@
 The `GNTParsings` module parsing internal representation.
 """
 struct parsing <: GNTParsings
-    # Part of speech
-    # ("A-", "C-", "D-", "I-", "N-", "P-", "RA", "RD", "RI", "RP", "RR", "V-", "X-")
-    pos::String
-    # Person
-    # ('-', '1', '2', '3')
-    per::Char
-    # Tense
-    # ('-', 'A', 'F', 'I', 'P', 'X', 'Y')
-    ten::Char
-    # Voice
-    # ('-', 'A', 'M', 'P')
-    voi::Char
-    # Mood
-    # ('-', 'D', 'I', 'N', 'O', 'P', 'S')
-    moo::Char
-    # Case
-    # ('-', 'A', 'D', 'G', 'N', 'V')
-    cas::Char
-    # Number
-    # ('-', 'P', 'S')
-    num::Char
-    # Gender
-    # ('-', 'F', 'M', 'N')
-    gen::Char
-    # Degree
-    # ('-', 'C', 'S')
-    deg::Char
+    pos::String     # Part of speech
+    per::Char       # Person
+    ten::Char       # Tense
+    voi::Char       # Voice
+    moo::Char       # Mood
+    cas::Char       # Case
+    num::Char       # Number
+    gen::Char       # Gender
+    deg::Char       # Degree
     # Internal (validating) constructor
     function parsing(pos::S, per::C, ten::C,
                      voi::C, moo::C, cas::C,
                      num::C, gen::C, deg::C) where {S<:AbstractString, C<:AbstractChar}
+        cat = "$pos $per$ten$voi$moo$cas$num$gen$deg"
         @assert(
-            pos in ("A-", "C-", "D-", "I-", "N-", "P-","V-", "X-") ||
-            pos in ("RA", "RD", "RI", "RP", "RR")
+            pos in ("A-","C-","D-","I-","N-","P-","RA","RD","RI","RP","RR","V-","X-"),
+            "'$cat': invalid part of speech '$pos'"
         )
-        @assert per in "-123"
-        @assert ten in "-AFIPXY"
-        @assert voi in "-AMP"
-        @assert moo in "-DINOPS"
-        @assert cas in "-ADGNV"
-        @assert num in "-PS"
-        @assert gen in "-FMN"
-        @assert deg in "-CS"
+        @assert(per in "-123"   , "'$cat': invalid person '$per'")
+        @assert(ten in "-AFIPXY", "'$cat': invalid tense  '$ten'")
+        @assert(voi in "-AMP"   , "'$cat': invalid voice  '$voi'")
+        @assert(moo in "-DINOPS", "'$cat': invalid mood   '$moo'")
+        @assert(cas in "-ADGNV" , "'$cat': invalid case   '$cas'")
+        @assert(num in "-PS"    , "'$cat': invalid number '$num'")
+        @assert(gen in "-FMN"   , "'$cat': invalid gender '$gen'")
+        @assert(deg in "-CS"    , "'$cat': invalid degree '$deg'")
         return new(pos, per, ten, voi, moo, cas, num, gen, deg)
     end
 end
@@ -54,7 +37,7 @@ end
 export parsing
 
 # External convenience constructors - two-arg splatting version
-parsing(pos::S, par::S) where S<:AbstractString = parsing(pos, par...)
+parsing(pos::X, par::Y) where {X<:AbstractString, Y<:AbstractString} = parsing(pos, par...)
 
 # External convenience constructors - one-arg, space-delimited version
 parsing(all::S) where S<:AbstractString = begin
